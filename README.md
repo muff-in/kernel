76a8ff6b70fd7b484d3e238ec21ce2a7455cb90d
muffin <notmuffin@tuta.io>
2022-08-17 11:05:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# rust-kernel
+
+# Notes
+https://rustosnotes.notion.site/Rust-OS-Notes-92d8abde19a444f8b001ae2a0a73822b
+
+# Tip: Don't use new version of crates
+
+Bare Metal binary ✅
+
+Basic kernel ✅
+
+Booting in QEMU ✅
+
+Target Specification ✅
+
+Using VGA text mode ✅
+
+Using macros ✅
+
+Using Tests ✅
+
+Exit QEMU ✅
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+#![no_std]
+#![no_main] 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle]
+
+pub extern "C" fn _start() -> ! {  
+
+    println!("Hello World");
+
+    #[cfg(test)]
+    test_main();
+
+
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_asssertion() {
+    print!("trivial_asssertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+
+}
